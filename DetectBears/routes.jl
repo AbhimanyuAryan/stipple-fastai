7c8d3447cec3c05f51ba90c9f3bb673f4776eab8
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-12-25 23:40:02 +0530

    Uploading UI

--- DetectBears/routes.jl
+++ DetectBears/routes.jl
@@ -1,31 +1,38 @@
 using Genie, Stipple, StippleUI
 
+#=== config ==#
+
+for m in [Genie, Stipple, StippleUI]
+  m.assets_config.host = "https://cdn.statically.io/gh/GenieFramework"
+end
+
+# WEB_TRANSPORT = Genie.WebChannels #Genie.WebThreads #
+
+@reactive mutable struct Model <: ReactiveModel
+end
 
 #== view ==#
 function ui(model)
-  page(
-    prepend = """
-    <style>
-      .rb {
-        border-right: 10pt solid #fff;
-      }
-    </style>
-    """,
+  page(model, partial = true, [
+    heading("Bear Detection Demo")
 
-    vm(model), class="container", [
-      heading("Detect Bear Type AI")
-
-      row([
-        cell([
-          p(button("Reverse"))
-        ])
+    row([
+      Html.div(class="col-md-12", [
+        uploader(label="Upload Image", :auto__upload, :multiple,
+        url="http://localhost:4444/upload")
       ])
+    ])
     ]
   )
 end
 
+
 #== server ==#
 
 route("/") do
-  mode = Model |> init
+  model = Model |> init
+
+  html(ui(model), context = @__MODULE__)
 end
+
+isrunning(:webserver) || up()
