5d297a791b6e8c1cc0c5fd6b50bbf9e18b572d60
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-12-26 01:03:10 +0530

    Uploading Image POST

--- DetectBears/routes.jl
+++ DetectBears/routes.jl
@@ -1,12 +1,10 @@
 using Genie, Stipple, StippleUI
+using Genie.Requests
 
-#=== config ==#
-
-for m in [Genie, Stipple, StippleUI]
-  m.assets_config.host = "https://cdn.statically.io/gh/GenieFramework"
-end
-
-# WEB_TRANSPORT = Genie.WebChannels #Genie.WebThreads #
+Genie.config.cors_headers["Access-Control-Allow-Origin"]  =  "*"
+Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
+Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
+Genie.config.cors_allowed_origins = ["*"]
 
 @reactive mutable struct Model <: ReactiveModel
 end
@@ -18,8 +16,8 @@ function ui(model)
 
     row([
       Html.div(class="col-md-12", [
-        uploader(label="Upload Image", :auto__upload, :multiple,
-        url="http://localhost:4444/upload")
+        uploader(label="Upload Image", :auto__upload, :multiple, method="POST",
+        url="http://localhost:8000/upload")
       ])
     ])
     ]
@@ -33,6 +31,10 @@ route("/") do
   model = Model |> init
 
   html(ui(model), context = @__MODULE__)
+end
+
+route("/upload", method = POST) do
+  @info filespayload()
 end
 
 isrunning(:webserver) || up()
