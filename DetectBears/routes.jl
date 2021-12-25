9cc01d9fa9a0766fef6b217dacfc6b34b767efd5
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-12-25 22:09:15 +0530

    setting up route

--- DetectBears/routes.jl
+++ DetectBears/routes.jl
@@ -1,5 +1,31 @@
-using Genie.Router
+using Genie, Stipple, StippleUI
+
+
+#== view ==#
+function ui(model)
+  page(
+    prepend = """
+    <style>
+      .rb {
+        border-right: 10pt solid #fff;
+      }
+    </style>
+    """,
+
+    vm(model), class="container", [
+      heading("Detect Bear Type AI")
+
+      row([
+        cell([
+          p(button("Reverse"))
+        ])
+      ])
+    ]
+  )
+end
+
+#== server ==#
 
 route("/") do
-  serve_static_file("welcome.html")
+  mode = Model |> init
 end
